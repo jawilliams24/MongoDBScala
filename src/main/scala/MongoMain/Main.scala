6480950e6b7bdb7dbd8f7d04591d0c98af3875bc
jawilliams24 <jwilliams9517@gmail.com>
2020-03-10 16:46:40 +0000

    Database CRUD updated

--- src/main/scala/MongoMain/Main.scala
+++ src/main/scala/MongoMain/Main.scala
@@ -1,58 +1,118 @@
 package MongoMain
 
 import org.mongodb.scala._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.postfixOps
 import scala.util.{Failure, Success}
 import MongoMain.Helpers._
+
+import scala.util.control.Breaks
 import org.mongodb.scala.model.Accumulators._
 import org.mongodb.scala.model.Sorts._
 import org.mongodb.scala.model.Filters._
 import org.mongodb.scala.model.Aggregates._
 import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.model.{BulkWriteOptions, DeleteOneModel, InsertOneModel, ReplaceOneModel, UpdateOneModel, WriteModel}
+
 
 object Main extends App {
   val mongoClient: MongoClient = MongoClient()
   val database: MongoDatabase = mongoClient.getDatabase("mydb")
   val collection: MongoCollection[Document] = database.getCollection("test")
-
+  val loop = new Breaks
   val doc: Document = Document("name" -> "MongoDB", "type" -> "database",
     "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
 
-  collection.deleteMany(gte("i", 1)).printHeadResult("Delete Result: ")
-  collection.deleteMany(gte("name", "MongoDB")).printHeadResult("Delete Result: ")
+  val userChoice = scala.io.StdIn.readLine("Please select an option from: create, read, update, delete, exit > ")
+  var connected = false
+  loop.breakable {
 
-  collection.insertOne(doc).results()
+    while (!connected) {
 
-  val documents = (1 to 100) map { i: Int => Document("i" -> i) }
+      userChoice match {
 
-  val insertObservable = collection.insertMany(documents)
+        case ("create") =>
+          collection.insertOne(doc).results()
+          loop.break()
 
-  val insertAndCount = for {
-    insertResult <- insertObservable
-    countResult <- collection.countDocuments()
-  } yield countResult
+        case ("read") =>
+          collection.find().printResults()
+          loop.break()
 
-  val futureResult = insertAndCount.headOption()
+        case ("update") =>
+          collection.updateOne(equal("i", 10), set("i", 110)).printHeadResult("Update Result: ")
+          loop.break()
 
-  futureResult.onComplete {
-    case Success(value) => println(s"total # of documents after inserting 100 small ones (should be 101):  $value")
-    case Failure(exception) => exception.printStackTrace()
+        case ("delete") =>
+          collection.deleteMany(gte("i", 1)).printHeadResult("Delete Result: ")
+          collection.deleteMany(gte("_id", 1)).printHeadResult("Delete Result: ")
+          collection.deleteMany(gte("name", "MongoDB")).printHeadResult("Delete Result: ")
+          collection.deleteOne(equal("i", 110)).printHeadResult("Delete Result: ")
+          loop.break()
+
+        case ("stop") =>
+          connected = true
+
+
+      }
+    }
+
+
   }
 
-  Thread.sleep(2000)
+  //
+  //
+  //  val documents = (1 to 100) map { i: Int => Document("i" -> i) }
+  //
+  //  val insertObservable = collection.insertMany(documents)
+  //
+  //  val insertAndCount = for {
+  //    insertResult <- insertObservable
+  //    countResult <- collection.countDocuments()
+  //  } yield countResult
+  //
+  //  val futureResult = insertAndCount.headOption()
+  //
+  //  futureResult.onComplete {
+  //    case Success(value) => println(s"total # of documents after inserting 100 small ones (should be 101):  $value")
+  //    case Failure(exception) => exception.printStackTrace()
+  //  }
+  //
+  //  Thread.sleep(2000)
+  //
+  //
+  //  collection.find(and(gt("i", 50), lte("i", 100))).printResults()
+  //
+  //  collection.find(exists("i")).sort(descending("i")).first().printHeadResult()
+  //
+  //  collection.find().projection(excludeId()).first().printHeadResult()
+  //
+  //  collection.aggregate(Seq(filter(gt("i", 0)),
+  //    project(Document("""{ITimes10: {$multiply: ["$i", 10]}}""")))
+  //  ).printResults()
+  //
+  //  collection.aggregate(List(group(null, sum("total", "$i")))).printHeadResult()
+  //
+  //  collection.find(equal("i", 71)).first().printHeadResult()
+  //
+  //  collection.find(gt("i", 50)).printResults()
+  //
+  //  collection.updateMany(lt("i", 100), inc("i", 100)).printHeadResult("Update Result: ")
+  //
+  //  val writes: List[WriteModel[_ <: Document]] = List(
+  //      InsertOneModel(Document("_id" -> 4)),
+  //      InsertOneModel(Document("_id"-> 5)),
+  //      InsertOneModel(Document("_id" -> 6)),
+  //      UpdateOneModel(Document("_id" -> 1), set("x", 2)),
+  //      DeleteOneModel(Document("_id" -> 2)),
+  //      ReplaceOneModel(Document("_id" -> 3), Document("_id" -> 3, "x" -> 4))
+  //  )
+  //
+  //  collection.bulkWrite(writes).printHeadResult("Bulk write results: ")
+  //
+  //  collection.bulkWrite(writes, BulkWriteOptions().ordered(false)).printHeadResult("Bulk write results (unordered): ")
 
-  collection.find().printResults()
 
-  collection.find(and(gt("i", 50), lte("i", 100))).printResults()
-
-  collection.find(exists("i")).sort(descending("i")).first().printHeadResult()
-
-  collection.find().projection(excludeId()).first().printHeadResult()
-
-  collection.aggregate(Seq(filter(gt("i", 0)),
-    project(Document("""{ITimes10: {$multiply: ["$i", 10]}}""")))
-  ).printResults()
-
-  collection.aggregate(List(group(null, sum("total", "$i")))).printHeadResult()
 }
